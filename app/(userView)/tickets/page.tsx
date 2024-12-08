9e0cb640c3b84f33b934b2509628a1ee10391134
Aahil <aahilahmed9761154@gmail.com>
2024-12-08 16:25:49 +0500

    ticket change

--- app/(userView)/tickets/page.tsx
+++ app/(userView)/tickets/page.tsx
@@ -3,15 +3,16 @@ import Image from 'next/image';
 
 const TicketsPage = () => {
   return (
-    <div className='h-screen bg-gray-100 flex flex-col overflow-x-clip'>
-        <div className='relative 2xl:h-[800px] lg:h-[450px] h-[450px] mx-10 my-14 flex justify-center bg-black rounded-2xl'>
-          <div className="font-poppins absolute mx-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 text-center">
-            <h1 className="drop-shadow-md text-[5rem] font-semibold font-dancing text-stone-50">
-                Tickets
-            </h1>
-            <p className="text-stone-100 italic">Live the moment with the activities available at the island.</p>
-          </div>
-          <Image alt="ride" className="w-full h-full object-cover bg-black blur-sm opacity-90 rounded-2xl" src={"/perry.jpg"} width={2000} height={0} />
+    <div className="h-full bg-gray-100 flex flex-col overflow-x-clip">
+      {/* Hero Section */}
+      <div className="2xl:h-[800px] lg:h-[450px] h-[450px] mx-10 my-14 flex justify-center bg-black rounded-2xl">
+        <div className="font-poppins absolute mx-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 text-center">
+          <h1 className="drop-shadow-md text-[5rem] font-semibold font-dancing text-stone-50">
+            Tickets
+          </h1>
+          <p className="text-stone-100 italic">
+            Where the fun begins!
+          </p>
         </div>
         <Image
           alt="ride"
